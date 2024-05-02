11e602ca818feef256a1111a01d67ba609e6580e
yemimaaaaa <yemimahah@gmail.com>
2024-05-03 02:02:56 +0700

    2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -121,11 +121,11 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
   export async function deleteInvoice(id: string) {
     throw new Error('Failed to Delete Invoice');
-    try {
-      await sql`DELETE FROM invoices WHERE id = ${id}`;
-      revalidatePath('/dashboard/invoices');
-      return { message: 'Deleted Invoice.' };
-    } catch (error) {
-      return { message: 'Database Error: Failed to Delete Invoice.' };
-    }
+    // try {
+    //   await sql`DELETE FROM invoices WHERE id = ${id}`;
+    //   revalidatePath('/dashboard/invoices');
+    //   return { message: 'Deleted Invoice.' };
+    // } catch (error) {
+    //   return { message: 'Database Error: Failed to Delete Invoice.' };
+    // }
   }
