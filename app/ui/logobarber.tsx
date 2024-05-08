0ae02dcb4079ddd3ffe67c376321c73d01a593fd
yemimaaaaa <yemimahah@gmail.com>
2024-05-08 22:50:10 +0700

    DONE_UGD_8

--- app/ui/logobarber.tsx
+++ app/ui/logobarber.tsx
@@ -6,7 +6,7 @@ export default function LogoBarber() {
     <div className={`${kanit.className} flex flex-row items-center leading-none text-white justify-content: flex-end`}>
       <p className="text-[45px] kanit">Atma Barbershop</p>
       <Image
-
+      
         src="/assets/logo_hero.png"
         alt="Logo Hero"
         width={65}
